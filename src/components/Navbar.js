2d2abf343349df2d2ef25514b3f81570451ac5f3
Olusola i.t <Holushawlar.it@gmial.com>
2022-09-08 06:10:47 +0100

    build homefix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,10 @@ import logo from "../Assets/blue.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
 } from "react-icons/ai";
 
-import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
