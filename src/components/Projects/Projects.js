0fe71eb955259f65b75127b8243f1dec59665487
Olusola i.t <Holushawlar.it@gmial.com>
2022-09-09 12:04:30 +0100

    projects add

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+import leaf from "../../Assets/Projects/bamiz.png";
+import emotion from "../../Assets/Projects/easelearn.png";
+import motion from "../../Assets/Projects/royal.png";
+import mot from "../../Assets/Projects/book.png";
+import editor from "../../Assets/Projects/intellmanage.png";
 import chatify from "../../Assets/Projects/artistvote.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import suicide from "../../Assets/Projects/resort.png";
+import bitsOfCode from "../../Assets/Projects/metright.png";
 
 function Projects() {
   return (
@@ -49,8 +51,8 @@ function Projects() {
               isBlog={false}
               title="MiniManage"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="https://github.com/Tolushawlar/intellmanage"
+              demoLink="https://intellmanage.herokuapp.com/"              
             />
           </Col>
 
@@ -61,7 +63,7 @@ function Projects() {
               title="Bamzi"
               description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
               ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              demoLink="https://bamzi.ivyarc.com/"
             />
           </Col>
 
@@ -71,32 +73,32 @@ function Projects() {
               isBlog={false}
               title="Hotel Resort"
               description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" 
+              ghLink="https://github.com/Tolushawlar/resort"
+              demoLink="https://bearesort.netlify.app/" 
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={mot}
               isBlog={false}
               title="SpreeFree"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              ghLink="https://github.com/Tolushawlar/spreefree"
+              demoLink="https://spreefree.netlify.app/" 
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={motion}
               isBlog={false}
               title="Royal Mobile"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/" 
+              ghLink="https://github.com/Tolushawlar/royal-mobile"
+              demoLink="https://royale-mobile.netlify.app/" 
             />
           </Col>
 
@@ -107,8 +109,8 @@ function Projects() {
               title="EaseLearn"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/" 
+              ghLink="https://github.com/Tolushawlar/easeLearn"
+              demoLink="https://easelearn.netlify.app/"
             />
           </Col>
         </Row>
